0ade138cb373aa794f11ffa18f1a6742592a7302
Abel Adane <contacttoabel@gmail.com>
2023-04-06 23:11:23 +0300

    added antd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-tailwind-ant';
+  deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30;
 }
